228f2d13c7a183d2a0c228d36a0ec016e19ac6ac
Ckp-knight <orenbashan20@gmail.com>
2026-09-09 10:33:54 +0300

    הוספת האפשרות של להפעיל ישר את התוכנה עם F5

--- obj/Debug/net9.0-windows/Musical-Encryption-Project_bfq5jqva_wpftmp.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/Musical-Encryption-Project_bfq5jqva_wpftmp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Musical-Encryption-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+752e83b7d8845908abc0912d27764a4fe039790e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+717839bf5917273933a4c9888d79edc6a11127bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Musical-Encryption-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Musical-Encryption-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
